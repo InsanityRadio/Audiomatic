754ad85425972a9bf7a63c72fa84555f61b6135f
Jamie Woods <jamie.woods@bbc.co.uk>
2018-01-26 23:31:16 +0000

    Don't send file in the initial upload, 302 to its path

--- app.rb
+++ app.rb
@@ -1,11 +1,10 @@
+require 'base64'
 require 'securerandom'
-require 'open-uri'
 require 'erb'
 require 'tmpdir'
 require 'sinatra'
 require 'sinatra/streaming'
 require 'tempfile'
-require 'date'
 
 configure do
 	use Rack::Session::Cookie, :secret => "some unique secret string here"
@@ -21,19 +20,15 @@ def check_csrf
 	raise 'Incorrect CSRF' if params['csrf'] != session['csrf'] or session['csrf'] == nil
 end
 
-@temp = Dir.tmpdir
-
 post '/download/' do 
 	
 	check_csrf
 
 	content_type 'audio/flac'
 
-	final = Tempfile.new('sts', @temp)
+	final = Tempfile.new('sts', Dir.tmpdir)
 	final.close
 	
-	p params[:file]
-
 	raise "Invalid file" if !params[:file][:filename]
 
 	file_name = params[:file][:filename]
@@ -46,15 +41,24 @@ post '/download/' do
 	`avconv -i "#{params[:file][:tempfile].path}" -f wav -acodec pcm_s16le -ac 2 - | stereo_tool_cmd - - -s ./config.sts | avconv -i - -c:a flac -f flac -y #{final.path}`
 
 	puts "Calling send_file on #{final.path}"
-	send_file final.path, { :disposition => 'attachment', :filename => "#{file_name}.processed.flac", :type => 'flac' }
+	redirect "/file/" + File.basename(final.path) + "?fn=" + Base64::urlsafe_encode64(file_name)
 
+end
+
+get '/file/:file' do | file |
+		
+	next if file[0..2] != "sts" or file.include? "/"
+	file_name = Base64::urlsafe_decode64(params[:fn]) rescue "untitled"
+	
+	send_file Dir.tmpdir + "/" + file, { :disposition => 'attachment', :filename => "#{file_name}.processed.flac", :type => 'flac' }
+	
 end
 
 Thread.new do | a |
 
 	loop do
 	
-		Dir.glob(@temp + "/sts*").each do | path |
+		Dir.glob(Dir.tmpdir + "/sts*").each do | path |
 			data = `lsof #{path}`
 			next if data.to_s.length > 5
 			# The file isn't being read, we can close it
